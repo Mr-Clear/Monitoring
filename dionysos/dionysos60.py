a106843802b59f9af480892b716c0cec1642e244
Thomas <git@thomas.klierlinge.de>
2025-03-22 17:33:56 +0100

    Don't fail when no update is available.

--- dionysos/dionysos60.py
+++ dionysos/dionysos60.py
@@ -6,12 +6,13 @@ parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_name + "/lib")
 
 import db
-from subprocess import check_output, CalledProcessError, PIPE
+from subprocess import run, CalledProcessError, PIPE
 
 try:
-    check_output(['yay', '-Sy'], stderr=PIPE)
-    yay = check_output(['yay', '-Qum'], stderr=PIPE).decode('utf-8')
-    packaes = db.DbData('dionysos', 'outdated_packages',  str(yay.count('\n')), yay)
+    yay = run(['yay', '-Qum'], stdout=PIPE, stderr=PIPE)
+    out = yay.stdout.decode('utf-8')
+    packaes = db.DbData('dionysos', 'outdated_packages', str(out.count('\n')), out)
+
 except Exception as e:
     packaes = db.DbData('dionysos', 'outdated_packages',  None, str(e))
 
