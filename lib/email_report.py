094b1923fde6d5353930db82c4249b8cfae90dba
Thomas <git@thomas.klierlinge.de>
2025-09-23 09:18:40 +0200

    Support UTF-8

--- lib/email_report.py
+++ lib/email_report.py
@@ -4,14 +4,17 @@ import smtplib
 
 from datetime import datetime
 from email.mime.text import MIMEText
+from email.header import Header
+from email import charset as email_charset
 import configparser
 
 import os
 
 _path = os.path.dirname(os.path.realpath(__file__))
 
-def send_email(sender: str, subject: str, message: str):
-    msg = MIMEText(message)
+def send_email(sender: str, subject: str, message: str, charset: str = 'utf-8'):
+    email_charset.add_charset(charset, email_charset.QP, email_charset.QP, charset)
+    msg = MIMEText(message, _subtype='plain', _charset=charset)
     msg['Subject'] = subject
     msg['Date'] = datetime.now().strftime('%a, %d %b %Y %H:%M:%S %z')
 
@@ -26,6 +29,6 @@ def send_email(sender: str, subject: str, message: str):
         server.starttls()
         server.login(config_default['username'], config_default['password'])
         server.sendmail(msg['From'], msg['To'], msg.as_string())
-    
+
 if __name__ == '__main__':
     send_email('Tester', 'Mäil Test', 'Test Content\nHeitzölrückstoßabdämpfung\n\nENDE')
